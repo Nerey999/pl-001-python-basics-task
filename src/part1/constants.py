358a608a8f1b00cd60f74583e82ac0c3cb424b20
Nerey <saburovyar.2008@mail.ru>
2026-09-24 18:09:18 +0300

    write all functions and fix some problems

--- src/part1/constants.py
+++ src/part1/constants.py
@@ -15,9 +15,6 @@ changing a width keeps everything else consistent.
 from typing import Final
 
 
-# TODO: замените заглушки (0) на корректные значения, см. TASK.md.
-# Все константы, зависящие от ёмкостей *_BITS, должны вычисляться из них.
-
 # Twitter's original Snowflake epoch: 2010-11-04 01:42:54.657 UTC.
 EPOCH_MS_DEFAULT: Final[int] = 1288834974657
 
@@ -30,9 +27,9 @@ NODE_ID_BITS: Final[int] = 10
 SEQUENCE_ID_BITS: Final[int] = 12
 
 # Largest value each field can hold.
-TIMESTAMP_MS_MAX: Final[int] = 2**TIMESTAMP_BITS
-NODE_ID_MAX: Final[int] = 2**NODE_ID_BITS
-SEQUENCE_ID_MAX: Final[int] = 2**SEQUENCE_ID_BITS
+TIMESTAMP_MS_MAX: Final[int] = 2**TIMESTAMP_BITS - 1
+NODE_ID_MAX: Final[int] = 2**NODE_ID_BITS - 1
+SEQUENCE_ID_MAX: Final[int] = 2**SEQUENCE_ID_BITS - 1
 
 # Здесь можно добавить собственные вспомогательные константы
 # (например, сдвиги полей при сборке идентификатора).
